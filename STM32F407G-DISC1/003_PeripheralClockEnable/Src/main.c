596e87381dab7a3e3ec77c00250bb643cf6048ab
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-11-22 21:10:21 +0800

    Add macros for ADC control register

--- STM32F407G-DISC1/003_PeripheralClockEnable/Src/main.c
+++ STM32F407G-DISC1/003_PeripheralClockEnable/Src/main.c
@@ -18,9 +18,10 @@
 
 #include <stdint.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+/* Macros for ADC Control Register */
+#define ADC_BASE_ADDR   			0x40012000UL
+#define ADC_CR1_REG_OFFSET 			0x04UL
+#define ADC_CR1_REG_ADDR  			(ADC_BASE_ADDR + ADC_CR1_REG_OFFSET )
 
 int main(void)
 {
