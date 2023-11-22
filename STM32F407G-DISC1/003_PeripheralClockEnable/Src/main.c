af316217a444c64f589d051b9783209b408cb9a8
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-11-22 21:30:22 +0800

    Set the ADC control register

--- STM32F407G-DISC1/003_PeripheralClockEnable/Src/main.c
+++ STM32F407G-DISC1/003_PeripheralClockEnable/Src/main.c
@@ -21,10 +21,16 @@
 /* Macros for ADC Control Register */
 #define ADC_BASE_ADDR   			0x40012000UL
 #define ADC_CR1_REG_OFFSET 			0x04UL
-#define ADC_CR1_REG_ADDR  			(ADC_BASE_ADDR + ADC_CR1_REG_OFFSET )
+#define ADC_CR1_REG_ADDR  			(ADC_BASE_ADDR + ADC_CR1_REG_OFFSET)
 
 int main(void)
 {
+
+	uint32_t *pAdcCr1Reg = (uint32_t*) ADC_CR1_REG_OFFSET;
+
+	/* Set the 8 - bit of the SCAN register */
+	*pAdcCr1Reg |= (1 << 8);
+
     /* Loop forever */
 	for(;;);
 }
