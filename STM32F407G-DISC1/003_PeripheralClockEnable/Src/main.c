cc17034614c5f80890cefe027431a0f68b692daa
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-11-22 21:47:05 +0800

    Add enabling the peripheral clock for ADC1

--- STM32F407G-DISC1/003_PeripheralClockEnable/Src/main.c
+++ STM32F407G-DISC1/003_PeripheralClockEnable/Src/main.c
@@ -23,12 +23,21 @@
 #define ADC_CR1_REG_OFFSET 			0x04UL
 #define ADC_CR1_REG_ADDR  			(ADC_BASE_ADDR + ADC_CR1_REG_OFFSET)
 
+/* Define the APB2 bus registers */
+#define RCC_BASE_ADDR              0x40023800UL
+#define RCC_APB2_ENR_OFFSET        0x44UL
+#define RCC_APB2_ENR_ADDR          (RCC_BASE_ADDR + RCC_APB2_ENR_OFFSET)
+
 int main(void)
 {
 
-	uint32_t *pAdcCr1Reg = (uint32_t*) ADC_CR1_REG_OFFSET;
+	uint32_t *pAdcCr1Reg = (uint32_t*) ADC_CR1_REG_ADDR;
+	uint32_t *pRccApb2Enr = (uint32_t*) RCC_APB2_ENR_ADDR;
 
-	/* Set the 8 - bit of the SCAN register */
+	/* Enable the peripheral clock for ADC1 */
+	*pRccApb2Enr |= (1 << 8);
+
+	/* Modify the ADC CR1 register (SCAN register) */
 	*pAdcCr1Reg |= (1 << 8);
 
     /* Loop forever */
